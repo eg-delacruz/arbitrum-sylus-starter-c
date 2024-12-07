4ed5c3a4e1b77a777901b76cb527c382b9f6c819
Gerardo De La Cruz <netoxas3107@hotmail.com>
2024-12-07 18:07:45 +0100

    Up to date

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,52 +1,87 @@
-"use client"
+"use client";
 
-import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs"
-import { useRkAccountModal } from "@/lib/rainbowkit"
-import { useAccount } from "wagmi"
+import { useState } from "react";
+import { createPublicClient, createWalletClient, http, parseAbi } from "viem";
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+import { useRkAccountModal } from "@/lib/rainbowkit";
+import { useAccount, useReadContract } from "wagmi";
+import Image from "next/image";
+
+import logo from "../assets/logo.png";
+
+const ADDRESS = "0x0946e9c45504660e98464a7364be9f636d748ef4";
+
+const ABI = parseAbi(["function hola_mundo() public returns (string)"]);
 
 export default function Container() {
-  const { openAccountModal } = useRkAccountModal()
-  const { isConnected } = useAccount()
+  const { openAccountModal } = useRkAccountModal();
+  const { isConnected } = useAccount();
+  const result = useReadContract({
+    address: ADDRESS,
+    functionName: "hola_mundo",
+    abi: ABI,
+  });
+
+  const [input, setInput] = useState("");
+
+  console.log({ result });
+  const click = () => {};
+
+  const connect = () => {
+    openAccountModal();
+  };
 
   return (
-    <section className="max-w-2xl mt-12 mx-auto">
-      <Tabs defaultValue="feed" className="w-full">
-        <TabsList className="w-full p-0 border border-b-2 grid grid-cols-2">
-          <TabsTrigger className="h-full" value="feed">
-            Everything
-          </TabsTrigger>
-          <TabsTrigger
+    <>
+      <p>{result.data || "Loading..."}</p>
+      <section className="max-w-2xl mt-12 mx-auto">
+        <div className="w-full flex justify-center mt-24">
+          <Image src={logo} alt="Logo" width={550} height={90} />
+        </div>
+
+        <br />
+
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setInput("");
+          }}
+          className="flex flex-col items-center"
+        >
+          <input
+            placeholder="DNI"
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="w-full p-4 border border-gray-200 rounded-lg shadow-xl"
+          />
+          <br />
+          <input
+            placeholder="Three words address"
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="w-full p-4 border border-gray-200 rounded-lg shadow-xl"
+          />
+          <button
             onClick={() => {
-              if (!isConnected) {
-                // Abrimos el modal de conectar wallet
-                // si no está conectado
-                openAccountModal?.()
-              }
+              connect();
             }}
-            className="h-full"
-            value="personal"
+            className="mt-4 p-4 bg-blue-500 text-white rounded-lg"
           >
-            My publications
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value="feed">
-          <div className="grid grid-cols-3 gap-3">
-            {Array.from({ length: 9 }).map((_, i) => (
-              <section className="p-4 border rounded-lg">
-                <nav className="flex items-center gap-1">
-                  <strong>0x03242</strong>
-                  <span className="opacity-70">posted</span>
-                </nav>
-                <p className="text-black/70 mt-2">
-                  This is the feed where you can see all the posts from
-                  everyone.
-                </p>
-              </section>
-            ))}
-          </div>
-        </TabsContent>
-        <TabsContent value="personal">My publications</TabsContent>
-      </Tabs>
-    </section>
-  )
+            Connect
+          </button>
+
+          <button
+            onClick={() => {
+              click();
+            }}
+            className="mt-4 p-4 bg-blue-500 text-white rounded-lg"
+          >
+            Click
+          </button>
+        </form>
+      </section>
+    </>
+  );
 }
