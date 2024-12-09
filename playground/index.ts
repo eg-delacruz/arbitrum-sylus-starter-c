409e975280be65d3fd4628a72c50af87cd874981
Jean <jptivan53@gmail.com>
2024-12-09 12:42:35 +0100

    CHANGE: Playground vuelve a ser Typescript

--- playground/index.ts
+++ playground/index.ts
@@ -5,16 +5,16 @@ import {
   parseAbi,
   keccak256,
   stringToBytes,
+  getContract
 } from "viem";
 import { arbitrumSepolia } from "viem/chains";
 import { privateKeyToAccount } from "viem/accounts";
 import "dotenv/config";
 
 const ABI = parseAbi([
-  "function hola_mundo() public view returns (string)",
-  "function set_value(bytes32) public",
   "function get_value() public view returns (uint256)",
-  "function storeHash(bytes32) public view returns (bytes32)",
+  "function checkOwnership(bytes32) public view returns (uint256)",
+  "function storeHash(bytes32) returns (uint256)"
 ]);
 
 const account = privateKeyToAccount((process as any).env.PRIVATE_KEY);
@@ -22,29 +22,47 @@ const account = privateKeyToAccount((process as any).env.PRIVATE_KEY);
 const client = createWalletClient({
   chain: arbitrumSepolia,
   transport: http(),
-  account,
+  account
 });
 
 const publicClient = createPublicClient({
   chain: arbitrumSepolia,
-  transport: http(),
+  transport: http()
 });
 
 // https://sepolia.arbiscan.io/address/const CONTRACT_ADDRESS = "0x46be8751225be83d7a9b97fec0214c53795d8477"
-const CONTRACT_ADDRESS = "0xf60be222d397f0be3b11dafc1175f070ddf3eb30";
+const CONTRACT_ADDRESS = "0x20682fb0d6673d73a1d24f70216d2468d5af9b93";
 
 async function read() {
-  const hash = keccak256(stringToBytes("Hello, world!"));
+  const hash = keccak256(stringToBytes("HOLA.MUNDO.MADRID"));
 
   const result = await publicClient.readContract({
     abi: ABI,
     address: CONTRACT_ADDRESS,
-    functionName: "storeHash",
-    args: [hash],
+    functionName: "checkOwnership",
+    args: [hash]
   });
 
   console.debug(`Contract: ${result}`);
 }
 
+async function write() {
+  const hash = keccak256(stringToBytes("HOLA.MUNDO.MADRID"));
+
+  try {
+    const result = await client.writeContract({
+      abi: ABI,
+      address: CONTRACT_ADDRESS,
+      functionName: "storeHash",
+      args: [hash],
+      account
+    });
+
+    console.debug(result);
+  } catch (e) {
+    console.log("Error", e);
+  }
+}
+
 read();
-// write()
+//write();
