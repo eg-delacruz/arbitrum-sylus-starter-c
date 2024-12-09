44dbb4d1183bf115ab9f8fb5c8b6cc8afd5e33bb
Jean <jptivan53@gmail.com>
2024-12-09 14:04:48 +0100

    TESTING

--- playground/index.ts
+++ playground/index.ts
@@ -5,16 +5,17 @@ import {
   parseAbi,
   keccak256,
   stringToBytes,
-  getContract
+  getContract,
+  toHex
 } from "viem";
 import { arbitrumSepolia } from "viem/chains";
 import { privateKeyToAccount } from "viem/accounts";
 import "dotenv/config";
 
 const ABI = parseAbi([
-  "function get_value() public view returns (uint256)",
-  "function checkOwnership(bytes32) public view returns (uint256)",
-  "function storeHash(bytes32) returns (uint256)"
+  "function hola_mundo(bytes32) public view returns (string)",
+  "function checkOwnership(bytes32) public view returns (string)",
+  "function storeHash(bytes32) external returns (string)"
 ]);
 
 const account = privateKeyToAccount((process as any).env.PRIVATE_KEY);
@@ -31,23 +32,30 @@ const publicClient = createPublicClient({
 });
 
 // https://sepolia.arbiscan.io/address/const CONTRACT_ADDRESS = "0x46be8751225be83d7a9b97fec0214c53795d8477"
-const CONTRACT_ADDRESS = "0x20682fb0d6673d73a1d24f70216d2468d5af9b93";
+const CONTRACT_ADDRESS = "0xfbe9ec3c152203dc2a34857d84b8bed540bd8a08";
 
 async function read() {
-  const hash = keccak256(stringToBytes("HOLA.MUNDO.MADRID"));
+  const hash = keccak256(toHex("HOLA.MUNDO.MADRID"));
 
-  const result = await publicClient.readContract({
-    abi: ABI,
-    address: CONTRACT_ADDRESS,
-    functionName: "checkOwnership",
-    args: [hash]
-  });
+  try {
+    const result = await publicClient.readContract({
+      abi: ABI,
+      address: CONTRACT_ADDRESS,
+      functionName: "hola_mundo",
+      args: [hash],
+      account
+    });
 
-  console.debug(`Contract: ${result}`);
+    console.debug(`Contract: ${result}`);
+  } catch (e) {
+    console.error(e);
+  }
+
+  console.log({ hash });
 }
 
 async function write() {
-  const hash = keccak256(stringToBytes("HOLA.MUNDO.MADRID"));
+  const hash = keccak256(toHex("HOLA.MUNDO.MADRID"));
 
   try {
     const result = await client.writeContract({
@@ -64,5 +72,8 @@ async function write() {
   }
 }
 
+//console.log({ ABI, inputs: ABI[2].inputs });
+
+//console.log({ account });
 read();
 //write();
